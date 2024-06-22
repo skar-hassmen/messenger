6982e19f6f7e8e6e9742aec907a0cb0623a44680
skarhassmen <andro.vaheev@gmail.com>
2024-06-22 23:30:17 +0300

    [feature] used setting params

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
 
+from api import router as api_router
+from core.config import settings
+
 app = FastAPI()
+app.include_router(
+    api_router,
+    prefix=settings.api.prefix
+)
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', reload=True)
+    uvicorn.run(
+        'main:app',
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True
+    )
